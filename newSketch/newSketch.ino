f038d606b1075fc6345659e46ca1b18f6064469b
Takana219 <2014.mimoza@ezweb.ne.jp>
2022-09-14 23:58:22 +0900

    newFact:  重くない送信ができた。改行問題の解決

--- newSketch/newSketch.ino
+++ newSketch/newSketch.ino
@@ -46,10 +46,6 @@ void task(void *pvParameters)
     float yawOffset = 0;           // ヨーのオフセット
     float yawOld = 0;              // 前回のヨー
     float yaw2 = 0;                // 補正後のヨー
-
-    float before_pitch = 0.0f;
-    float before_roll = 0.0f;
-    float before_yaw = 0.0f;
     while (1)
     {
         int8_t data;
@@ -59,10 +55,6 @@ void task(void *pvParameters)
         float accX;
         float accY;
         float accZ;
-
-        
-
-        
         // タイマー割り込みがあるまで待機する
         xQueueReceive(xQueue, &data, portMAX_DELAY);
         M5.IMU.getAhrsData(&pitch, &roll, &yaw);
@@ -100,7 +92,7 @@ void task(void *pvParameters)
                 pitchSum = 0;
                 rollSum = 0;
                 yawSum = 0;
-                Serial.printf("Calibration Init!!!!! %f\n", gyro);
+//                Serial.printf("Calibration Init!!!!! %f\n", gyro);
             }
             else
             {
@@ -117,12 +109,12 @@ void task(void *pvParameters)
                     pitchOffset = pitchSum / calibrationCount;
                     rollOffset = rollSum / calibrationCount;
                     yawOffset = yawSum / calibrationCount;
-                    Serial.printf(" pitchSum = %f\n", pitchSum);
-                    Serial.printf(" rollSum = %f\n", rollSum);
-                    Serial.printf(" yawSum = %f\n", yawSum);
-                    Serial.printf(" pitchOffset = %f\n", pitchOffset);
-                    Serial.printf(" rollOffset = %f\n", rollOffset);
-                    Serial.printf(" yawOffset = %f\n", yawOffset);
+//                    Serial.printf(" pitchSum = %f\n", pitchSum);
+//                    Serial.printf(" rollSum = %f\n", rollSum);
+//                    Serial.printf(" yawSum = %f\n", yawSum);
+//                    Serial.printf(" pitchOffset = %f\n", pitchOffset);
+//                    Serial.printf(" rollOffset = %f\n", rollOffset);
+//                    Serial.printf(" yawOffset = %f\n", yawOffset);
                 }
             }
         }
@@ -150,34 +142,33 @@ void task(void *pvParameters)
             //================================================================================================
 
 
-            M5.MPU6886.getAccelData(&accX, &accY, &accZ);  //加速度取得
-
-            Serial.printf(" %f,%f,%f\n", pitch, roll, yaw2);
+//            Serial.printf(" %f,%f,%f\n", pitch, roll, yaw2);
             M5.Lcd.printf("%.2f  %.2f  %.2f\n", pitch, roll, yaw2);
             //シリアル通信
-
-            float difference_pitch = pitch - before_pitch;
-            float difference_roll = roll - before_roll ;
-            float difference_yaw = yaw2 - before_yaw ;
-            
-            SerialBT.printf("x%.2f,y%.2f,z%.2f", difference_pitch, difference_roll, difference_yaw);
-            SerialBT.printf(",a%.2f,b%.2f,c%.2f", accX, accY, accZ);
-
-            before_pitch = pitch;
-            before_roll = roll;
-            before_yaw = yaw2;
+//            SerialBT.printf("x%.2fy%.2fz%.2f", pitch, roll, yaw2);
+//            Serial.printf("x%.2fy%.2fz%.2f", pitch, roll, yaw2);
+//            SerialBT.printf("a%.2fb%.2fc%.2f", accX, accY, accZ);
+//            Serial.printf("a%.2fb%.2fc%.2f", accX, accY, accZ);
 
             //光センサー(数字が小さいと明るい, 数字が大きいと暗い)
             uint16_t analogRead_value = analogRead(33);
             int isLightCheck = lightSensor(analogRead_value);
             // シリアルモニタに”1"と文字列を送信
-            SerialBT.printf(",l%d\n", isLightCheck);
+//            SerialBT.printf("l%d\n", isLightCheck);
+//            Serial.printf("l%d\n", isLightCheck);
+
+            Serial.printf("x%.2f,y%.2f,z%.2f,a%.2f,b%.2f,c%.2f,l%d\r\n", pitch, roll, yaw2, accX, accY, accZ, isLightCheck);
+            SerialBT.printf("x%.2f,y%.2f,z%.2f,a%.2f,b%.2f,c%.2f,l%d\r\n", pitch, roll, yaw2, accX, accY, accZ, isLightCheck);
+            //Serial.printf("x%.2f,y%.2f,z%.2f,a%.2f,b%.2f,c%.2f,l%d\n", pitch, roll, yaw2, accX, accY, accZ, isLightCheck);
+            //SerialBT.printf("x%.2f,y%.2f,z%.2f,a%.2f,b%.2f,c%.2f,l%d\n", pitch, roll, yaw2, accX, accY, accZ, isLightCheck);
             digitalWrite(10, isLightCheck); //LED をオンオフさせる。
+            
 
             //グラフ化
             String str = "pitch:"+String(pitch) + "," +"roll:" + String(roll) + "," + "yaw:" + String(yaw2);
-            Serial.println(str);
+//            Serial.println(str);
             //SerialBT.println(str);
+            
 
 
         }
@@ -191,14 +182,14 @@ void setup()
     M5.IMU.Init();
     M5.Lcd.setRotation(3); //モニタ画面方向設定
     M5.MPU6886.Init();     //センサ初期化
-//    Serial.begin(9600);  //シリアル通信初期化
-//    Serial.begin(115200); //シリアル通信初期化
-    //Serial.begin(20000);
+    Serial.begin(9600);  //シリアル通信初期化
+    //Serial.begin(115200); //シリアル通信初期化
+    //Serial.begin(4800);
     pinMode(32, INPUT);   //光センサーの読み取り
     pinMode(10, OUTPUT);  // LED
 
     // Bluetooth初期化（デバイス名）
-    SerialBT.begin("M5STICK-C-TGSPET");
+    SerialBT.begin("M5STICK-C-TGSPET2");
 
     // キュー作成
     xQueue = xQueueCreate(1, sizeof(int8_t));
